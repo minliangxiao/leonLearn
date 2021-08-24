2a34dbdf468f2ab7f99c02d9e0d0a1812905ca0f
growyb <1782907800@qq.com>
2021-08-24 23:23:27 +0800

    反射+自定义注解+aop完成限流框架，完成

--- src/main/java/com/example/alip6/annotationAndReflection/service/MemberService.java
+++ src/main/java/com/example/alip6/annotationAndReflection/service/MemberService.java
@@ -1,19 +1,20 @@
 package com.example.alip6.annotationAndReflection.service;
 
 import com.example.alip6.annotationAndReflection.annotation.GrowYb;
+import com.example.alip6.annotationAndReflection.annotation.Limiter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberService {
     @GetMapping("get")
-    @GrowYb(name = "get",token = 1)
+    @Limiter(name = "get",token = 1)
     public String get(){
         return "my is get";
     }
 
     @GetMapping("add")
-    @GrowYb(name = "add",token = 5)
+    @Limiter(name = "add",token = 5)
     public String add(){
         return "my is add";
     }
