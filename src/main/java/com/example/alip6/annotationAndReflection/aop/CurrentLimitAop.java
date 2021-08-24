c7886bf5d4d7f237467d110e27a8ae6129a12a1e
growyb <1782907800@qq.com>
2021-08-24 23:13:43 +0800

    反射+自定义注解+aop完成限流框架，完成

--- src/main/java/com/example/alip6/annotationAndReflection/aop/CurrentLimitAop.java
+++ src/main/java/com/example/alip6/annotationAndReflection/aop/CurrentLimitAop.java
@@ -1,9 +1,12 @@
 package com.example.alip6.annotationAndReflection.aop;
 
+import com.example.alip6.annotationAndReflection.annotation.Limiter;
 import com.google.common.util.concurrent.RateLimiter;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,7 +22,36 @@ public class CurrentLimitAop {
 
     @Around(value = "@annotation(com.example.alip6.annotationAndReflection.annotation.Limiter)")
     public Object around(ProceedingJoinPoint joinPoint){
+        // 走环绕通知
+        try{
+            // 获取拦截方法
+            Signature sig = joinPoint.getSignature();
+            // 获取拦截的方法名（反射获取）
+            MethodSignature methodSignature = (MethodSignature) sig;
+            Limiter limiter = methodSignature.getMethod().getDeclaredAnnotation(Limiter.class);
+            // 获取到该注解的name
+            String name = limiter.name();
+            // 获取到该注解的token
+            double token = limiter.token();
+            RateLimiter rateLimiter = rateLimiters.get(name);
+            if (rateLimiter == null){
+                rateLimiter = RateLimiter.create(token);
+                rateLimiters.put(name,rateLimiter);
+            }
+            /*
+            * 调用guava的方法执行限流相关操作
+            * */
+            boolean result = rateLimiter.tryAcquire();
+            if (!result){
+                return "当前访问人数过多，请稍后重试";
+            }
+            // 执行目标方法
+            Object proceed = joinPoint.proceed();
+            return proceed;
+        }catch (Throwable throwable) {
+            throwable.printStackTrace();
+            return "系统错误";
+        }
 
-        return null;
     }
 }
